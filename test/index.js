f39b5b883ef9368d3b2a7ab7cee11cb78ba2ace7
Arnout Kazemier <info@3rd-Eden.com>
2015-05-06 14:40:35 +0200

    [test] Adding minimal test runner

---
+++ test/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var Mocha = require('mocha')
+  , mochify = require('mochify');
+
+/**
+ * Poor mans kill switch. Kills all active hooks.
+ *
+ * @api private
+ */
+function kill() {
+  require('async-each')(kill.hooks, function each(fn, next) {
+    fn(next);
+  }, function done(err) {
+    if (err) return process.exit(1);
+
+    process.exit(0);
+  });
+}
+
+/**
+ * All the hooks that need destruction.
+ *
+ * @type {Array}
+ * @private
+ */
+kill.hooks = [];
+
+//
+// This is the magical test runner that setup's all the things and runs various
+// of test suites until something starts failing.
+//
+(function runner(steps) {
+  if (!steps.length) return kill(), runner;
+
+  var step = steps.shift();
+
+  step(function unregister(fn) {
+    kill.hooks.push(fn);
+  }, function register(err) {
+    if (err) throw err;
+
+    runner(steps);
+  });
+
+  return runner;
+})([
+  //
+  // Start-up a small static file server so we can download files and fixtures
+  // inside our PhantomJS test.
+  //
+  require('./static'),
+
+  //
+  // Run the PhantomJS tests now that we have a small static server setup.
+  //
+  function phantomjs(kill, next) {
+    mochify('./test/*.browser.js', {
+      reporter: 'spec',
+      cover: true,
+      ui: 'bdd'
+    })
+    .bundle(next);
+  }
+]);
---
+++ test/static.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var fs = require('fs')
+  , url = require('url')
+  , path = require('path')
+  , http = require('http');
+
+module.exports = function staticserver(kill, next) {
+  var server = http.createServer(function serve(req, res) {
+    var file = path.join(__dirname, url.parse(req.url).pathname);
+
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (!fs.existsSync(file)) {
+      res.statusCode = 404;
+      return res.end('nope');
+    }
+
+    res.statusCode = 200;
+    fs.createReadStream(file).pipe(res);
+  });
+
+  kill(function close(next) {
+    server.close(next);
+  });
+
+  server.listen(8080, next);
+};
+
+//
+// Static server loaded directly.
+//
+if (require.main === module) module.exports(function kill() {
+}, function next(err) {
+  if (err) throw err;
+
+  console.log('static server listening on ', this.address());
+});

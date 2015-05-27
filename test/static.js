bb26b29c23764772272f9d7c1b1a8b126ba1017b
Arnout Kazemier <info@3rd-Eden.com>
2015-05-27 17:18:17 +0200

    [fix] Overrdie the CSP header

--- test/static.js
+++ test/static.js
@@ -13,11 +13,12 @@ module.exports = function staticserver(kill, next) {
   var server = http.createServer(function serve(req, res) {
     var file = path.join(__dirname, url.parse(req.url).pathname);
 
-    setHeader(res, 'Access-Control-Allow-Origin', req.headers.origin || 'http://localhost');
+    setHeader(res, 'Access-Control-Allow-Origin', req.headers.origin);
     setHeader(res, 'Access-Control-Allow-Credentials', 'true');
 
     if (!fs.existsSync(file)) {
       req.headers.host = '';
+      setHeader(res, 'Content-Security-Policy', 'removed');
       return proxy.web(req, res, { target: 'https://raw.githubusercontent.com' });
     }
 
